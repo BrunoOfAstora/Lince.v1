d008877224c6242634675b480ac66da387d96ce9
BrunoOfAstora <brunopb1234567@hotmail.com>
2024-10-07 17:34:11 -0300

    Test 2, check later

--- src/main/java/Lince/JaguarUI.java
+++ src/main/java/Lince/JaguarUI.java
@@ -1,32 +1,67 @@
 package Lince;
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
+import java.awt.*;
+import java.awt.event.ActionListener;
 import java.io.File;
 
 public class JaguarUI extends JFrame {
-
     public JaguarUI() {
 
-        File file = new File("/home/");
+        //Acessa o diretório principal (Home)
+        String userHome = System.getProperty("user.home");
+        File file = new File("/home/groundzero/Documents");
 
+        if(!file.exists()){
+            System.out.println("Directory not found" + userHome);
+            return;
+        }else{
+            System.out.println("Directory accessed: " + userHome);
+        }
+
+        File[] files = file.listFiles();
+        if(files == null || files.length == 0){
+            System.out.println("Nothing found in: " + userHome);
+        }else{
+            System.out.println("Files found in userhome: " + userHome);
+            for(File f : files){
+                System.out.println(f.getName());
+            }
+        }
+
+        //Ajustes da janela do Programa
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("Jaguar");
         this.pack();
         this.setSize(800, 600);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
+        this.getContentPane().setBackground(Color.DARK_GRAY);
 
+        JToolBar jToolBar = new JToolBar();
+
+        JButton nextButton = new JButton("Next");
+        JButton deleteButton = new JButton("Delete");
+
+        jToolBar.add(nextButton);
+        jToolBar.add(deleteButton);
+
+        Container pane = this.getContentPane();
+        pane.add(jToolBar, BorderLayout.NORTH);
+
+        //Root Node da JTree
         DefaultMutableTreeNode treeNodes = new DefaultMutableTreeNode(file.getPath());
 
         directories(treeNodes,file);
 
         JTree tree = new JTree(treeNodes);
-        tree.setBounds(25, 25, 200, 100);
-
         JScrollPane scrollPane = new JScrollPane(tree);
-        this.add(scrollPane);
 
-        this.add(tree);
+        tree.setBackground(Color.DARK_GRAY);
+        scrollPane.setBackground(Color.DARK_GRAY);
+
+        this.add(scrollPane);
+        this.setVisible(true);
 
     }
 
@@ -43,4 +78,5 @@ public class JaguarUI extends JFrame {
         }
     }
 }
+
 
