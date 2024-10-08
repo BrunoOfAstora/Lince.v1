9ba993c43729a89de53cf2a9abb0150d5b935c39
BrunoOfAstora <brunopb1234567@hotmail.com>
2024-10-08 16:19:14 -0300

    Test 2, check later

--- src/main/java/Lince/JaguarUI.java
+++ src/main/java/Lince/JaguarUI.java
@@ -1,6 +1,7 @@
 package Lince;
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeNode;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -12,6 +13,7 @@ public class JaguarUI extends JFrame {
         String userHome = System.getProperty("user.home");
         File file = new File("/home/groundzero/Documents");
 
+        //Verifica se o Caminho existe
         if(!file.exists()){
             System.out.println("Directory not found" + userHome);
             return;
@@ -19,6 +21,7 @@ public class JaguarUI extends JFrame {
             System.out.println("Directory accessed: " + userHome);
         }
 
+        //Verifica se há algum arquivo ou pasta no caminho
         File[] files = file.listFiles();
         if(files == null || files.length == 0){
             System.out.println("Nothing found in: " + userHome);
@@ -38,31 +41,53 @@ public class JaguarUI extends JFrame {
         this.setVisible(true);
         this.getContentPane().setBackground(Color.DARK_GRAY);
 
+        //Inicialização da ToolBar
         JToolBar jToolBar = new JToolBar();
 
+        //Conf. dos Botões
         JButton nextButton = new JButton("Next");
         JButton deleteButton = new JButton("Delete");
 
+        //Conf. da Toolbar
         jToolBar.add(nextButton);
         jToolBar.add(deleteButton);
-
         Container pane = this.getContentPane();
         pane.add(jToolBar, BorderLayout.NORTH);
 
+
         //Root Node da JTree
         DefaultMutableTreeNode treeNodes = new DefaultMutableTreeNode(file.getPath());
-
         directories(treeNodes,file);
-
         JTree tree = new JTree(treeNodes);
         JScrollPane scrollPane = new JScrollPane(tree);
 
+        //Personalização da Janela
         tree.setBackground(Color.DARK_GRAY);
         scrollPane.setBackground(Color.DARK_GRAY);
 
+        //Obtém o caminho do objeto selecionado
+        DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) tree.getLastSelectedPathComponent();
+        if(selectedNode == null){
+            return;
+        }
+        File fileDelete = new File(getFullPath(selectedNode));
+
+        //"Seta" a visibilidade da janela e adiciona um sistema de rolgaem caso a pagina seja muito grande
         this.add(scrollPane);
         this.setVisible(true);
+    }
 
+    //Método para obter o caminho do objeto selecionado
+    public String getFullPath(DefaultMutableTreeNode node){
+        StringBuilder fullPath = new StringBuilder();
+        TreeNode[] nodes = node.getPath();
+        for(int i = 0; i < nodes.length; i++){
+            fullPath.append(nodes[i].toString());
+            if(i < nodes.length - 1){
+                fullPath.append(File.separator);
+            }
+        }
+        return fullPath.toString();
     }
 
     public void directories(DefaultMutableTreeNode dir, File file){
